7974b1c302062f44c5375a4bd705eac34f7c5970
Steven Boutcher <boutchersj@gmail.com>
2024-08-25 11:40:46 -0500

    Verify empty input fields

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,20 +1,21 @@
 let username = "lambdaTester";
 let password = "Tester01";
 
-describe("User Login Flow", () => {
-  beforeEach(() => {
-    cy.intercept("POST", "https://api.demoblaze.com/login").as("loginAPI");
-  });
-  it("user logs in successfully with valid credentials", () => {
+describe("Login flow", () => {
+  it("should allow the user to log in with valid credentials", () => {
+    // Arrange
     cy.visit("https://www.demoblaze.com/");
     cy.get("#login2").click();
     cy.get("#logInModal").should("be.visible");
+    cy.get("#loginusername").should("be.empty");
+    cy.get("#loginpassword").should("be.empty");
+  
+    // Act
     cy.get("#loginusername").clear().type(username, { delay: 50 });
     cy.get("#loginpassword").clear().type(password, { delay: 50 });
     cy.get(".btn-primary").contains("Log in").click();
-    cy.wait("@loginAPI").then((object) => {
-      expect(object.response.statusCode).to.eq(200);
-    });
+
+    // Assert
     cy.get("#nameofuser").should("contain", username);
     cy.get("#logout2").should("contain", "Log out");
   });
